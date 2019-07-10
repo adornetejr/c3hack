ce9849cf13847c7df7923e5abd6436cbb700fca8
Adornete Martins Jr <adornetejr@gmail.com>
2019-07-10 17:57:39 -0300

    Update index.html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,18 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('a[href="#top"]').fadeIn();
+      } else {
+          $('a[href="#top"]').fadeOut();
+      }
+  });
+
+  $('a[href="#top"]').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+  });
+});
+
 $(document).ready(function onDocumentReady() {
     var animation = bodymovin.loadAnimation({
       container: document.getElementById('illustration'),
